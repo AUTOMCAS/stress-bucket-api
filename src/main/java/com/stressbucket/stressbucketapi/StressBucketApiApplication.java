3b815137c796264a2bc6e44bbcd3682606004ef4
Chris <caston88@gmail.com>
2023-03-09 16:06:46 +0000

    Add CORS filter

--- src/main/java/com/stressbucket/stressbucketapi/StressBucketApiApplication.java
+++ src/main/java/com/stressbucket/stressbucketapi/StressBucketApiApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class StressBucketApiApplication {
@@ -13,6 +16,20 @@ public class StressBucketApiApplication {
 		SpringApplication.run(StressBucketApiApplication.class, args);
 	}
 
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter() {
+		FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		source.registerCorsConfiguration("/**", config);
+		registrationBean.setFilter(new CorsFilter(source));
+		registrationBean.setOrder(0);
+		return registrationBean;
+
+
+	}
 	@Bean
 	public FilterRegistrationBean<AuthFilter> filterFilterRegistrationBean(){
 		FilterRegistrationBean<AuthFilter> registrationBean = new FilterRegistrationBean<>();
